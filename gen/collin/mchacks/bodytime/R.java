968aa190fd075db554757ba0d586b628666ae05f
CollinStubbs <collin.stubbs@uoit.net>
2014-02-22 20:16:51 -0500

    Fixed up alarms

--- gen/collin/mchacks/bodytime/R.java
+++ gen/collin/mchacks/bodytime/R.java
@@ -31,9 +31,10 @@ public final class R {
         public static final int mainborder=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int action_settings=0x7f090004;
         public static final int alarm=0x7f090001;
         public static final int gymfind=0x7f090002;
+        public static final int layout1=0x7f090003;
         public static final int title=0x7f090000;
     }
     public static final class layout {
--- src/collin/mchacks/bodytime/MainActivity.java
+++ src/collin/mchacks/bodytime/MainActivity.java
@@ -1,15 +1,20 @@
 package collin.mchacks.bodytime;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -21,6 +26,9 @@ public class MainActivity extends Activity implements OnClickListener {
 	               "KiloGram_KG.otf");
 		 TextView title = (TextView) findViewById(R.id.title);
 	     title.setTypeface(t);
+	     
+	     Button alarm = (Button)findViewById(R.id.alarm);
+	     alarm.setOnClickListener(this);
 	 
 	}
 
@@ -31,13 +39,25 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
+	@SuppressLint("ResourceAsColor")
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
 		switch(arg0.getId()){
 		case R.id.alarm:
+			LinearLayout layout = (LinearLayout)findViewById(R.id.layout1);
+			layout.setGravity(Gravity.CENTER_HORIZONTAL);
 			
-			break;
+			TextView alarmSet = new TextView(this);
+			alarmSet.setText("What time does your\n gym open and close?");
+			alarmSet.setTextColor(R.color.black);
+			alarmSet.setTextSize(22);
+			alarmSet.setPadding(0,20,0,20);
+			layout.addView(alarmSet);
+			
+			TimePicker openTime = new TimePicker(this);
+			layout.addView(openTime);
+					break;
 		case R.id.gymfind:
 			break;
 		}
