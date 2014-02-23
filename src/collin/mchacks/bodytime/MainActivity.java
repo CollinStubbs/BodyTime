3d59f9fc74ce38d9871466053033a8a736ea2c61
CollinStubbs <collin.stubbs@uoit.net>
2014-02-22 20:35:55 -0500

    alarms good but button colour

--- src/collin/mchacks/bodytime/MainActivity.java
+++ src/collin/mchacks/bodytime/MainActivity.java
@@ -2,6 +2,7 @@ package collin.mchacks.bodytime;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -11,13 +12,16 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class MainActivity extends Activity implements OnClickListener {
 
+
+public class MainActivity extends Activity implements OnClickListener {
+	LinearLayout layout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,6 +33,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	     
 	     Button alarm = (Button)findViewById(R.id.alarm);
 	     alarm.setOnClickListener(this);
+	     
+	     layout = (LinearLayout)findViewById(R.id.layout1);
 	 
 	}
 
@@ -39,24 +45,56 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
-	@SuppressLint("ResourceAsColor")
+	@SuppressLint({ "ResourceAsColor", "NewApi" })
 	@Override
 	public void onClick(View arg0) {
+		layout.removeAllViewsInLayout();
 		// TODO Auto-generated method stub
 		switch(arg0.getId()){
 		case R.id.alarm:
-			LinearLayout layout = (LinearLayout)findViewById(R.id.layout1);
+			
 			layout.setGravity(Gravity.CENTER_HORIZONTAL);
 			
 			TextView alarmSet = new TextView(this);
 			alarmSet.setText("What time does your\n gym open and close?");
 			alarmSet.setTextColor(R.color.black);
 			alarmSet.setTextSize(22);
-			alarmSet.setPadding(0,20,0,20);
+			alarmSet.setPadding(25,20,0,20);
 			layout.addView(alarmSet);
+			
+			TextView alarmO = new TextView(this);
+			alarmO.setText("Opening:");
+			alarmO.setTextColor(R.color.black);
+			alarmO.setTextSize(17);
+			alarmO.setPadding(20,20,0,10);
+			layout.addView(alarmO);
 			
 			TimePicker openTime = new TimePicker(this);
 			layout.addView(openTime);
+			
+			TextView alarmC = new TextView(this);
+			alarmC.setText("Closing:");
+			alarmC.setTextColor(R.color.black);
+			alarmC.setTextSize(17);
+			alarmC.setPadding(20,20,0,10);
+			layout.addView(alarmC);
+			
+			TimePicker closeTime = new TimePicker(this);
+			closeTime.setPadding(0,0,0,85);
+			layout.addView(closeTime);
+			
+			RelativeLayout.LayoutParams rel_btn = new RelativeLayout.LayoutParams(
+		            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			
+			Button set = new Button(this);
+			set.setPadding(10, 10, 10, 10);
+			set.setLayoutParams(rel_btn);
+			set.setBackgroundColor(R.color.title_blue);
+			set.setTextColor(R.color.white);
+			set.setTextSize(20);
+			set.setText("Set Reminder Alarm");
+			layout.addView(set);
+			//set.setOnClickListener();
 					break;
 		case R.id.gymfind:
 			break;
